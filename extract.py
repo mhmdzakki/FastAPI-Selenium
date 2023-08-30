511dc43ef3e571ccf3a28734f3f9a98daccf8940
mhmdzakki <zakkim07@gmail.com>
2023-08-30 21:47:50 +0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from webdriver_manager.chrome import ChromeDriverManager
+
+import telebot
+import os
+import time
+
+
+bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+
 
 def createDriver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
@@ -17,9 +30,56 @@ def createDriver() -> webdriver.Chrome:
 
     return myDriver
 
-def getGoogleHomepage(driver: webdriver.Chrome) -> str:
-    driver.get("https://www.google.com")
-    return driver.page_source
+def autoAbsensi(driver: webdriver.Chrome) -> str:
+    # driver.get("https://www.google.com")
+    # return driver.page_source
+    driver.set_window_size(375, 812)  # iPhone X size
+
+
+    url = os.getenv("URL")
+
+    # Opening the website
+    driver.get(url)
+
+    npm = driver.find_element(By.NAME, "username")
+    npm.send_keys(os.getenv('NPM'))
+
+    pw = driver.find_element(By.NAME, "password")
+    pw.send_keys(os.getenv('PASSWORD'))
+
+    pw.send_keys(Keys.ENTER)
+
+    menu = driver.find_element(By.ID, "mobile-collapse")
+
+    menu.click()
+
+    list = driver.find_elements(By.CLASS_NAME, "pcoded-item")
+
+    for x in list:
+        li = x.find_elements(By.TAG_NAME, "li")
+        li[1].click()
+    
+    while True:
+        try:
+            button = WebDriverWait(driver, 60).until(
+                EC.presence_of_element_located((By.ID, "konfirmasi-kehadiran")))
+            button.click()
+            time.sleep(1)  # Delay for 1 minute (60 seconds).
+            bot.send_message(os.getenv('GROUP_ID'), 'Berhasil absen')
+
+            print('berhasil absen')
+            confirm = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "confirm")))
+            confirm.send_keys(Keys.ENTER)
+            confirm.click()
+        except:
+            print('belum waktunya absen')
+            bot.send_message(os.environ['GROUP_ID'], 'belum waktunya absen')
+            driver.refresh()
+            pass
+        else:
+            continue
+    
 
 def doBackgroundTask(inp):
     print("Doing background task")
